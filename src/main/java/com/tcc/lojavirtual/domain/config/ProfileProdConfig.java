dedb169aa9a3dc8a759231bee33083d1dfa2c80a
daniel.cavalcanti@outlook.com.br <daniel.cavalcanti@outlook.com.br>
2018-03-05 06:02:54 -0300

    Test Prod 1.0

---
+++ src/main/java/com/tcc/lojavirtual/domain/config/ProfileProdConfig.java
@@ -0,0 +1,37 @@
+package com.tcc.lojavirtual.domain.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.tcc.lojavirtual.service.EmailService;
+import com.tcc.lojavirtual.service.ProfileTestDados;
+import com.tcc.lojavirtual.service.SmtpEmailService;
+
+@Configuration
+@Profile("prod")
+public class ProfileProdConfig {
+	
+	@Autowired
+	private ProfileTestDados profileTestDados;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String create;
+	
+	
+	@Bean
+	public boolean instanciarBD() throws Exception {
+		
+		if(!create.equals("create")) return false;
+		
+		profileTestDados.insereDados();
+		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+}
